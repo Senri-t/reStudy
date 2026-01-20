d172045891ba84da52d7b4e4f7eee69ca679fb73
Senri-t <izoyata@gmail.com>
2026-01-20 12:38:36 +0900

    Update UserRequest.java

--- MybatisPractice/src/main/java/com/example/demo/dto/UserRequest.java
+++ MybatisPractice/src/main/java/com/example/demo/dto/UserRequest.java
@@ -2,6 +2,8 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Data;
 
 /**
@@ -15,15 +17,18 @@ public class UserRequest implements Serializable {
 	/**
 	 * 名前
 	 */
+	@NotEmpty(message = "名前を入力してください")
 	private String name;
 
 	/**
 	 * 住所
 	 */
+	@NotEmpty(message = "住所を入力してください")
 	private String address;
 
 	/**
 	 * 電話番号
 	 */
+	@NotEmpty(message = "電話番号を入力してください")
 	private String phone;
 }
